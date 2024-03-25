7aabc37425b87984fc87837070477c5730348988
David A Roberts <d@vidr.cc>
2024-03-25 20:44:24 +1000

    wire it to top

--- TestPattern/top.v
+++ TestPattern/top.v
@@ -20,10 +20,10 @@ module top (
   );
 
   wire [7:0] red, grn, blu;
-  wire [23:0] pixel;
-  assign red= pixel[23:16];
-  assign grn= pixel[15:8];
-  assign blu= pixel[7:0];
+  // wire [23:0] pixel;
+  // assign red= pixel[23:16];
+  // assign grn= pixel[15:8];
+  // assign blu= pixel[7:0];
 
   wire o_red;
   wire o_grn;
@@ -57,12 +57,24 @@ module top (
   else if (o_rd)
     hcount <= hcount + 1'b1;
 
-  shader shader_instance(
-    .hcount(hcount),
-    .vcount(vcount),
-    .red(pixel[23:16]),
-    .green(pixel[15:8]),
-    .blue(pixel[7:0])
+  PanoCore PanoCore_instance(
+    .io_pix_x(hcount - 320),
+    .io_pix_y(240 - vcount),
+    .io_pixel_in_vsync(hcount == 0 && vcount == 0),
+    .io_pixel_in_req(o_rd),
+    // io.pixel_out.eol    := pixel_active ? last_col                | False
+    .io_pixel_in_eol(o_rd && hcount == 639),
+    // io.pixel_out.eof    := pixel_active ? (last_col && last_line) | False
+    .io_pixel_in_eof(o_rd && hcount == 639 && vcount == 479),
+    .io_pixel_in_pixel_r(0),
+    .io_pixel_in_pixel_g(255),
+    .io_pixel_in_pixel_b(0),
+    // .io_pixel_out_req(o_de),
+    .io_pixel_out_pixel_r(red),
+    .io_pixel_out_pixel_g(grn),
+    .io_pixel_out_pixel_b(blu),
+    .clk(clk_25MHz),
+    .reset(reset)
   );
 
   OBUFDS OBUFDS_red(.I(o_red), .O(gpdi_dp[2]), .OB(gpdi_dn[2]));
